f3d867e0a85966d2938172640949ee99bf55622d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-31 12:01:40 +0100

    Property kezdő értékeke a StudentDto-ban

--- Kreta.Shared/Dtos/StudentDto.cs
+++ Kreta.Shared/Dtos/StudentDto.cs
@@ -4,12 +4,12 @@ namespace Kreta.Shared.Dtos
 {
     public class StudentDto
     {
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime BirthsDay { get; set; }
-        public int SchoolYear { get; set; }
-        public SchoolClassType SchoolClass { get; set; }
-        public string EducationLevel { get; set; }
+        public Guid Id { get; set; } = Guid.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; }=string.Empty;
+        public DateTime BirthsDay { get; set; } = DateTime.MinValue;
+        public int SchoolYear { get; set; } = 0;
+        public SchoolClassType SchoolClass { get; set; } = SchoolClassType.ClassA;
+        public string EducationLevel { get; set; } = String.Empty;
     }
 }
