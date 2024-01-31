4de64d9b706003b93ef92da65f1172aa673c587d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-31 11:59:21 +0100

    StudentDto konstruktorok törlése

--- Kreta.Shared/Dtos/StudentDto.cs
+++ Kreta.Shared/Dtos/StudentDto.cs
@@ -11,27 +11,5 @@ namespace Kreta.Shared.Dtos
         public int SchoolYear { get; set; }
         public SchoolClassType SchoolClass { get; set; }
         public string EducationLevel { get; set; }
-
-        public StudentDto(Guid id, string firstName, string lastName, DateTime birthsDay, int schoolYear, SchoolClassType schoolClass, string educationLevel)
-        {
-            Id = id;
-            FirstName = firstName;
-            LastName = lastName;
-            BirthsDay = birthsDay;
-            SchoolYear = schoolYear;
-            SchoolClass = schoolClass;
-            EducationLevel = educationLevel;
-        }
-
-        public StudentDto()
-        {
-            Id = Guid.NewGuid();
-            FirstName = string.Empty;
-            LastName = string.Empty;
-            BirthsDay = new DateTime();
-            SchoolYear = 9;
-            SchoolClass = SchoolClassType.ClassA;
-            EducationLevel = string.Empty;
-        }
     }
 }
